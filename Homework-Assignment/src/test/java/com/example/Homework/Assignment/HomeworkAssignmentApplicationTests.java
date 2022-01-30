81965903118264ae83021001d7aa658775a27769
BaibaIvane <baibaiivaane@gmail.com>
2022-01-30 20:08:41 +0200

    Initial commit

---
+++ Homework-Assignment/src/test/java/com/example/Homework/Assignment/HomeworkAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Homework.Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
