815f7ecc4bf27e46041f0df4ea1f90ef4062be83
BaibaIvane <baibaiivaane@gmail.com>
2022-01-30 22:51:56 +0200

    develop: Data migration using FlyWay

---
+++ Homework-Assignment/src/main/resources/db.migration/V1_AddDatabaseTables.sql
@@ -0,0 +1,7 @@
+CREATE TABLE books(
+    id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255),
+    author VARCHAR(255),
+    description VARCHAR(255),
+    PRIMARY KEY (id)
+);
---
+++ Homework-Assignment/src/main/resources/db.migration/V2_InsertDataBooks.sql
@@ -0,0 +1,5 @@
+INSERT INTO books
+VALUES
+(null, 'Big Ed', 'Peter Parker', '...'),
+(null, 'Long way to Norway', 'Filip Vangok', '...'),
+(null, 'She took my soul', 'Lana Flower', '...');
