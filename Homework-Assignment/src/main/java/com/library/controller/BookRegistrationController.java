450a25abf2a6d72f3bd64826d9cd7c0257b96aeb
BaibaIvane <baibaiivaane@gmail.com>
2022-02-03 20:34:10 +0200

    develop: Deleted redundant files

--- Homework-Assignment/src/main/java/com/library/controller/BookRegistrationController.java
+++
@@ -1,8 +0,0 @@
-package com.library.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BookRegistrationController {
-
-}
--- Homework-Assignment/src/main/resources/db.migration/V2_InsertDataBooks.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO books
-VALUES
-(null, 'Big Ed', 'Peter Parker', '...'),
-(null, 'Long way to Norway', 'Filip Vangok', '...'),
-(null, 'She took my soul', 'Lana Flower', '...');
